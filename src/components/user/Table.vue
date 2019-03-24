a749c74d5844a6b57cb325c182d0e2f770db7347
astatsuya <sharekobekko@gmail.com>
2019-03-24 23:39:47 +0900

    使わないコードを削除

--- src/components/user/Table.vue
+++ src/components/user/Table.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="handson" :style="{ 'margin-top': '10px', height: '200px', width: '1500px' }">
     <button @click="showData">data</button>
-    {{tableData}}
     <HotTable :data="currentAllUsersInfo" :settings="hotSettings" ref="hotTableComponent"/>
   </div>
 </template>
@@ -27,13 +26,7 @@ export default {
     };
   },
   computed: {
-    ...mapState([
-      "allUsersInfo",
-      "allCompanyGroupInfo",
-      "newUsers",
-      "deleteUsers",
-      "allCheck"
-    ]),
+    ...mapState(["allUsersInfo", "allCompanyGroupInfo", "newUsers"]),
     hotSettings: function() {
       return {
         // data: [],
@@ -46,62 +39,28 @@ export default {
           "dropdown",
           "dropdown"
         ],
-        // columns: function(column) {
-        //   let columnMeta = {};
-
-        //   if (column === 0) {
-        //     columnMeta.data = "checkbox";
-        //     columnMeta.type = "checkbox";
-        //   } else if (column === 1) {
-        //     columnMeta.data = "login_id";
-        //     columnMeta.type = "text";
-        //     columnMeta.validator = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        //     // columnMeta.readOnly = true;
-        //   } else if (column === 2) {
-        //     columnMeta.data = "name";
-        //     columnMeta.type = "text";
-        //   } else if (column === 3) {
-        //     columnMeta.data = "kana";
-        //     columnMeta.type = "text";
-        //     columnMeta.validator = /^[あ-ん゛゜ぁ-ぉゃ-ょー「」、]+/;
-        //   } else if (column === 4) {
-        //     columnMeta.data = "is_company_user";
-        //     columnMeta.type = "dropdown";
-        //     columnMeta.source = ["企業", "ユーザー"];
-        //     // columnMeta.strict = true;
-        //   } else if (column === 5) {
-        //     columnMeta.data = "company_group_id";
-        //     columnMeta.type = "dropdown";
-        //   } else if (column === 6) {
-        //     columnMeta.data = "status";
-        //     columnMeta.type = "dropdown";
-        //   } else {
-        //     columnMeta = null;
-        //   }
-        //   return columnMeta;
-        // },
         columns: [
           { data: "checkbox", type: "checkbox" },
           {
             data: "code",
             placeholder: "ログインID",
             type: "text",
-            allowEmpty: false,
-            validator: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)(?<!.{100,})$/
+            allowEmpty: false
+            // validator: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)(?<!.{100,})$/
           },
           {
             data: "description",
             placeholder: "ユーザー名",
             allowEmpty: false,
-            type: "text",
-            validator: /^.{1,50}$/
+            type: "text"
+            // validator: /^.{1,50}$/
           },
           {
             data: "rate",
             type: "numeric",
             placeholder: "かな",
-            allowEmpty: false,
-            validator: /^[あ-ん゛゜ぁ-ぉゃ-ょー「」、]+/
+            allowEmpty: false
+            // validator: /^[あ-ん゛゜ぁ-ぉゃ-ょー「」、]+/
           },
           {
             data: "is_company_user",
@@ -117,7 +76,7 @@ export default {
             allowEmpty: false,
             handsontable: {
               // ここになんらかの方法でapiからのデータを入れたい
-              data: this.columnSources()
+              // data: [this.$store.getters.newGroupsInfo]
             }
           },
           {
@@ -125,16 +84,9 @@ export default {
             type: "dropdown",
             placeholder: "ここの選択肢",
             allowEmpty: false,
-            source: this.columnSources()
+            source: [this.$store.getters.newGroup]
             // ここになんらかの方法でapiからのデータを入れたい
           },
-          // {
-          //   data: "company_group_id",
-          //   type: "dropdown",
-          //   placeholder: "企業グループ",
-          //   allowEmpty: false,
-          //   source: ["企業", "user"]
-          // },
           {
             data: "status",
             type: "dropdown",
@@ -157,12 +109,9 @@ export default {
             }
           }
         },
-        cells: this.cellsVue,
         minSpareRows: 1,
         beforeCreateRow: this.beforeCreateRowVue,
-        beforeRemoveRow: this.beforeRemoveRowVue,
         afterBeginEditing: this.afterBeginEditingVue
-        // language: "en-EN",
       };
     },
     currentAllUsersInfo: {
@@ -173,35 +122,12 @@ export default {
         this.setAllUsersInfo(value);
       }
     },
-    currentAllCompanyGroupInfo: {
-      get() {
-        return this.allCompanyGroupInfo;
-      },
-      set(value) {
-        this.setCompanyGroupApiData(value);
-      }
-    },
-    currentAllCheck: {
-      get() {
-        return this.allCheck;
-      },
-      set(value) {
-        this.setAllCheck(value);
-      }
-    },
-    currentDeleteUsers: {
-      get() {
-        return this.deleteUsers;
-      },
-      set(value) {
-        this.setDeleteUsers(value);
-      }
+    columnSources: function() {
+      // console.log(this);
+      // console.log(this.newUsers);
+      // console.log(this.$store.getters.newGroupsInfo.code);
+      return [{ ty: "fds" }, { ty: "jest" }];
     }
-    // columnSources: function() {
-    //   console.log(this);
-    //   console.log(this.newUsers);
-    //   return [{ ty: "test" }, { ty: "jest" }];
-    // }
   },
   mounted: function() {
     this.getManagementApi();
@@ -216,27 +142,21 @@ export default {
     ]),
     ...mapActions(["getManagementApi"]),
     // ここにapiのデータが入れられない
-    columnSources: function() {
-      console.log(this);
-      // これはvuexに最初からあるstate
-      console.log(this.newUsers);
-      // これがエラーになる。vuexでapiから取得したデータを入れたstate
-      // 実際には別のapiから取得したデータを入れたい
-      // console.log(this.currentAllUsersInfo);
-      return [{ ty: "test" }, { ty: "jest" }];
-    },
+    // columnSources: function() {
+    //   console.log(this);
+    //   // これはvuexに最初からあるstate
+    //   console.log(this.newUsers);
+    //   // これがエラーになる。vuexでapiから取得したデータを入れたstate
+    //   // 実際には別のapiから取得したデータを入れたい
+    //   // console.log(this.currentAllUsersInfo);
+    //   return [{ ty: "test" }, { ty: "jest" }];
+    // },
     beforeCreateRowVue: function(index, amount, source) {
       const newUsers = Object.assign({}, this.newUsers);
       this.currentAllUsersInfo.push(newUsers);
     },
-    beforeRemoveRowVue: function(index) {
-      if (this.currentAllUsersInfo[index].registeredUser) {
-        this.currentDeleteUsers.push(this.currentAllUsersInfo[index]);
-      }
-      this.currentAllUsersInfo.splice(index, 1);
-    },
     afterBeginEditingVue: function(row) {
-      console.log(this.newUsers);
+      // console.log(this.newUsers);
       this.currentAllUsersInfo[row].input = true;
     },
     showData: function() {
@@ -244,33 +164,6 @@ export default {
 
       this.tableData = this.$refs.hotTableComponent.hotInstance.getData();
     }
-    // changeAllCheckbox: function() {
-    //   this.allCheck !== true
-    //     ? this.currentAllUsersInfo.map(value => (value.checkbox = true))
-    //     : this.currentAllUsersInfo.map(value => (value.checkbox = false));
-    // },
-    // cellsVue: function(row, col, prop) {
-    //   let cellProperties = {};
-    //   // if (row === 0) {
-    //   //   cellProperties.readOnly = true;
-    //   // }
-    //   if (this.currentAllUsersInfo[row].registeredUser && prop === "login_id") {
-    //     cellProperties.readOnly = true;
-    //   }
-    //   if (
-    //     this.currentAllUsersInfo[row].registeredUser &&
-    //     prop === "is_company_user"
-    //   ) {
-    //     cellProperties.readOnly = true;
-    //   }
-    //   if (
-    //     this.currentAllUsersInfo[row].registeredUser &&
-    //     prop === "company_group_id"
-    //   ) {
-    //     cellProperties.readOnly = true;
-    //   }
-    //   return cellProperties;
-    // }
   }
 };
 </script>
--- src/components/user/module.js
+++ src/components/user/module.js
@@ -6,10 +6,8 @@ const moduleUserManagement = {
   state: {
     getApi: "https://api.coindesk.com/v1/bpi/currentprice.json",
     // getCompanyGrouApi: process.env.VUE_APP_API_ADMINS_COMPANIES_GROUP,
-    // postApi: process.env.VUE_APP_API_ADMINS_ADMINS_USERS_BULK_IMPORT,
 
     allUsersInfo: [],
-    allCompanyGroupInfo: [],
     newUsers: {
       empty: false,
       input: false,
@@ -21,77 +19,44 @@ const moduleUserManagement = {
       rate: "",
       code: "",
       registeredUser: false
-    },
-    deleteUsers: [],
-    allCheck: false
+    }
   },
   mutations: {
     setAllUsersInfo: function(state, value) {
       state.allUsersInfo = value;
     },
-    setDeleteUsers: function(state, value) {
-      state.deleteUsers = value;
-    },
     setManagementApiData: function(state, payload) {
-      console.log(payload.getManagement);
-      // console.log(payload.getCompanyGroup);
-      // payload.getManagement.map(array => array.)
-      // payload.getManagement.unshift(state.colHeaders);
       state.allUsersInfo = [payload.getManagement];
-      // const addNewUsers = JSON.parse(JSON.stringify(state.newUsers));
-      // const initialView = payload.getManagement.concat(addNewUsers);
-
-      // state.allUsersInfo = initialView;
     },
     setCompanyGroupApiData: function(state, payload) {
-      // console.log(payload.getCompanyGroup);
       state.allCompanyGroupInfo = payload.getCompanyGroup;
     }
   },
-  getters: {},
+  getters: {
+    newGroupsInfo: state => value => {
+      console.log(state.allUsersInfo);
+      return state.allUsersInfo;
+    }
+  },
   actions: {
-    // 非同期処理あっているか確認が必要
     getManagementApi: async function(context) {
       const payload = {
-        getManagement: null,
-        getCompanyGroup: null
+        getManagement: null
       };
       const apiManagementData = () => {
         return api.get(context.state.getApi);
       };
-      // const apiCompanyGroupData = () => {
-      //   return api.get(context.state.getCompanyGrouApi);
-      // };
-      try {
-        const getApiData = await axios.all([
-          apiManagementData()
-          // apiCompanyGroupData()
-        ]);
-        // console.log(await getApiData[0].data.bpi);
-        const usersInfo = await getApiData[0].data.bpi.EUR;
-        // await usersInfo.map(value => {
-        //   value.is_company_user = context.getters.getStringByIsCompanyUser(
-        //     value.is_company_user
-        //   );
-        // value.status = context.getters.getStringByStatus(value.status);
 
+      try {
+        const getApiData = await axios.all([apiManagementData()]);
+        const usersInfo = await getApiData[0].data.bpi.EUR;
         (usersInfo.checkbox = await false),
           (usersInfo.registeredUser = await true),
           (usersInfo.input = await false),
           (usersInfo.empty = await false);
-        // });
 
         payload.getManagement = await usersInfo;
-        // console.log(await payload.getManagement);
         await context.commit("setManagementApiData", payload);
-
-        // ここからグループ
-        // if (getApiData[1].status === 200) {
-        //   const companyGroups = await getApiData[1].data.company_groups;
-        //   payload.getCompanyGroup = await companyGroups;
-
-        //   await context.commit("setCompanyGroupApiData", payload);
-        // }
       } catch (error) {
         alert(error);
       }
